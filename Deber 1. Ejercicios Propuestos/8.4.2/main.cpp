f0d6cc1478b2b026254e1e473dfc6a67eaebe512
josuealeman17 <eljossam@hotmail.com>
2021-12-22 21:44:24 -0500

    Deber 1

---
+++ Deber 1. Ejercicios Propuestos/8.4.2/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+#define DIMENSION 33;
+
+using namespace std;
+
+
+void iniciarArreglo();
+
+int main(int argv, char** arcg) {
+
+	cout << "EJERCICIO 8.4.2" << endl << endl;
+
+
+
+
+
+
+
+
+	return 0;
+}
+
+void iniciarArreglo() {
+	int dim;
+}
